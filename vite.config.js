0b104157ae89fcfe0a258be2802bd2393224fba4
avisstian <avistianhua@gmail.com>
2024-10-09 00:15:33 +0800

    first commit

---
+++ vite.config.js
@@ -0,0 +1,54 @@
+import { defineConfig } from "vite";
+import { ViteEjsPlugin } from "vite-plugin-ejs";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
+import { glob } from "glob";
+
+import liveReload from "vite-plugin-live-reload";
+
+function moveOutputPlugin() {
+  return {
+    name: "move-output",
+    enforce: "post",
+    apply: "build",
+    async generateBundle(options, bundle) {
+      for (const fileName in bundle) {
+        if (fileName.startsWith("pages/")) {
+          const newFileName = fileName.slice("pages/".length);
+          bundle[fileName].fileName = newFileName;
+        }
+      }
+    },
+  };
+}
+
+export default defineConfig({
+  // base ???å¯«æ??ï¼?
+  // base: '/CozyBeauty/'
+  base: "/web-layout-training-vite/",
+  plugins: [
+    liveReload(["./layout/**/*.ejs", "./pages/**/*.ejs", "./pages/**/*.html"]),
+    ViteEjsPlugin(),
+    moveOutputPlugin(),
+  ],
+  server: {
+    // ?????? server ??????è¨­é??????????????
+    open: "pages/index.html",
+  },
+  build: {
+    rollupOptions: {
+      input: Object.fromEntries(
+        glob
+          .sync("pages/**/*.html")
+          .map((file) => [
+            path.relative(
+              "pages",
+              file.slice(0, file.length - path.extname(file).length)
+            ),
+            fileURLToPath(new URL(file, import.meta.url)),
+          ])
+      ),
+    },
+    outDir: "dist",
+  },
+});
